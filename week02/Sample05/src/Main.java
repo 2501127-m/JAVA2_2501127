a0ae3b2b4848401441b6d2e36274bffc2e11a5b8
milana <frantseva928@kbu.ac.kr>
2026-03-15 23:38:38 +0900

    week02

---
+++ week02/Sample05/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Person lee = new Person("이순신", 27, '남', new Club("테니스", 5));
+
+        System.out.println(lee);
+
+    }
+}
