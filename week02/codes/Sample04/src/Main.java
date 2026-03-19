5c28d44736c7aec41cf92830982c61a7a8ce20de
2501127-m <frantseva928@kbu.ac.kr>
2026-03-19 14:19:56 +0900

    Delete week02 directory

--- week02/codes/Sample04/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Person kim = new Person("김철수", new Date(2005, 3, 20), 183, 64.3f);
-
-        System.out.println(kim);
-
-    }
-}
